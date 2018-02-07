29153687682f25a0166a1ef617923bbc4c19f094
unknown <alyciarileym@gmail.com>
2018-02-06 20:41:45 -0600

    OMDB is close to being finished

--- liri.js
+++ liri.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var request = require("request");
 var spotify = require("node-spotify-api");
 var twitter = require("twitter");
+var omdb = require("omdb");
 var inquirer = require("inquirer");
 
 
@@ -15,36 +16,32 @@ var params = "";
 for (i = 3; i < process.argv.length; i++) {
     params = params + " " + process.argv[i];
 }
-//console.log(process.argv);
-//console.log(operation);
-//console.log(params);
+
 
 switch (operation) {
     case "my-tweets":
         doTweets();
         break;
     case "spotify-this-song":
-        doSpotify();
+        doSpotify(params);
         break;
     case "movie-this":
-        doIMDB();
+        doIMDB(params);
         break;
     case "do-what-it-says":
         doRandom();
         break;
 
 }
-//my-tweets function: show last 20 tweets and when they were created
-//create stubs - mark a place where you need to go to add things to
+//tweets is working don't touch it
 
 function doTweets() {
-    /*console.log("doTweets is under construction");*/
     var client = new twitter({
         consumer_key: process.env.TWITTER_CONSUMER_KEY,
         consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
         access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
         access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-    });
+    })
 
     var trumpTweets = {
         screen_name: "realdondahlen"
@@ -56,7 +53,7 @@ function doTweets() {
                 console.log(tweets[i].created_at);
                 console.log(tweets[i].text);
                 console.log("*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*");
-                            
+
             }
         } else {
             console.log("error");
@@ -73,25 +70,19 @@ function doSpotify() {
 }
 
 function doIMDB() {
-    console.log("doIMDB is under construction");
+
+    request("http://www.omdbapi.com/?t=" + params + "=&plot=short&apikey=trilogy", function (error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Released: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+        }
+    });
+
 }
 
 function doRandom() {
     console.log("doRandom is under construction");
 }
-
-
-
-//spotify-this-song:  shows artist, songs name, preview link of song, album song is from, defaults to Ace of Base
-
-
-//movie-this: title of movie, year movie came out, IMDB rating of movie, Rotten Tomatoes rating of movie, country where movie was produced, language of movie, plot of movie, actors in movie
-
-/*request("https://www.omdbapi.com/&y=&plot=short&apikey=trilogy", function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-      console.log("The movie's IMDB rating is:  " + JSON.parse(body).imdbRating);
-  }
-});*/
-
-//title, year,imdbRating, 
-//do-what-it-says:  will run the text inside of random.txt
